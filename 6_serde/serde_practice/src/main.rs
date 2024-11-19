45998c439174c75d21f3a8d5305dd6990f0936e1
x0lotl <danil200319771@gmail.com>
2024-11-19 19:41:50 +0200

    feat(serde): serialize json from file

--- 6_serde/serde_practice/src/main.rs
+++ 6_serde/serde_practice/src/main.rs
@@ -1,23 +1,74 @@
-use serde::{Serialize, Deserialize};
-use serde_json;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::Read;
+use std::time::Duration;
+use url::Url;
+use uuid::Uuid;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct User {
-    name: String,
-    email: String,
-    birthdate: String,
+#[derive(Debug, Serialize, Deserialize)]
+struct PublicTariff {
+    id: u32,
+    price: u32,
+    #[serde(with = "humantime_serde")]
+    duration: Duration,
+    description: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct PrivateTariff {
+    client_price: u32,
+    #[serde(with = "humantime_serde")]
+    duration: Duration,
+    description: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Stream {
+    user_id: Uuid,
+    is_private: bool,
+    settings: u32,
+    shard_url: Url,
+    public_tariff: PublicTariff,
+    private_tariff: PrivateTariff,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Gift {
+    id: u32,
+    price: u32,
+    description: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Debug {
+    #[serde(with = "humantime_serde")]
+    duration: Duration,
+    at: DateTime<Utc>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum RequestType {
+    #[serde(rename = "success")]
+    Success,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Request {
+    #[serde(rename = "type")]
+    request_type: RequestType,
+    stream: Stream,
+    gifts: Vec<Gift>,
+    debug: Debug,
 }
 
 fn main() {
-    let user = User {
-        name: "Danylo".to_string(),
-        email: "danil200319771@gmail.com".to_string(),
-        birthdate: "2003-10-10".to_string(),
-    };
+    let mut file = File::open("request.json").unwrap();
+    let mut json_str = String::new();
+    file.read_to_string(&mut json_str).unwrap();
 
-    let json = serde_json::to_string(&user).expect("Serialize JSON failed");
-    println!("Serialized JSON: {}", json);
+    let request: Request = serde_json::from_str(&json_str).unwrap();
 
-    let deserialized_user: User = serde_json::from_str(&json).expect("Deserialize JSON failed");
-    println!("Deserialized JSON: {:?}", deserialized_user);
+    println!("Request: {:?}", request);
 }
+
