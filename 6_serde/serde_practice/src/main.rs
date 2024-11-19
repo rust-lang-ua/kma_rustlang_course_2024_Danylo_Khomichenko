990b0c77b91c499f4c10c33c18af9253a9e085d5
x0lotl <danil200319771@gmail.com>
2024-11-19 20:23:06 +0200

    feat(serde): custom serialization

--- 6_serde/serde_practice/src/main.rs
+++ 6_serde/serde_practice/src/main.rs
@@ -1,85 +1,37 @@
-mod test;
-
-use chrono::{DateTime, Utc};
+use serde::{Serializer, Deserializer};
 use serde::{Deserialize, Serialize};
-use serde_yaml::to_string as to_yaml;
-use std::fs::File;
-use std::io::Read;
-use std::time::Duration;
-use toml::to_string as to_toml;
-use url::Url;
-use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize)]
-struct PublicTariff {
-    id: u32,
-    price: u32,
-    #[serde(with = "humantime_serde")]
-    duration: Duration,
-    description: String,
+struct Event {
+    name: String,
+    #[serde(serialize_with = "serialize_date", deserialize_with = "deserialize_date")]
+    date: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct PrivateTariff {
-    client_price: u32,
-    #[serde(with = "humantime_serde")]
-    duration: Duration,
-    description: String,
+fn serialize_date<S>(date: &str, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    serializer.serialize_str(&format!("Date super custom: {}", date))
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Stream {
-    user_id: Uuid,
-    is_private: bool,
-    settings: u32,
-    shard_url: Url,
-    public_tariff: PublicTariff,
-    private_tariff: PrivateTariff,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Gift {
-    id: u32,
-    price: u32,
-    description: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Debug {
-    #[serde(with = "humantime_serde")]
-    duration: Duration,
-    at: DateTime<Utc>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-enum RequestType {
-    #[serde(rename = "success")]
-    Success,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Request {
-    #[serde(rename = "type")]
-    request_type: RequestType,
-    stream: Stream,
-    gifts: Vec<Gift>,
-    debug: Debug,
+fn deserialize_date<'de, D>(deserializer: D) -> Result<String, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: &str = Deserialize::deserialize(deserializer)?;
+    Ok(s.replace("Date super custom: ", ""))
 }
 
 fn main() {
-    let mut file = File::open("request.json").unwrap();
-    let mut json_str = String::new();
-    file.read_to_string(&mut json_str).unwrap();
+    let event = Event {
+        name: "Event1".to_string(),
+        date: "2024-10-10".to_string(),
+    };
 
-    let request: Request = serde_json::from_str(&json_str).unwrap();
+    let json = serde_json::to_string(&event).expect("Serialization error");
+    println!("Serialized JSON with custom data: {}", json);
 
-    println!("Request: {:?}", request);
-
-    let yaml_str = to_yaml(&request).unwrap();
-    println!("YAML:\n{}", yaml_str);
-
-    let toml_str = to_toml(&request).unwrap();
-    println!("TOML:\n{}", toml_str);
-
+    let deserialized_event: Event = serde_json::from_str(&json).expect("Deserialization error");
+    println!("Serialized JSON with custom data: {:?}", deserialized_event);
 }
-
