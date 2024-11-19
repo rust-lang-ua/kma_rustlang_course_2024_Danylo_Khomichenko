37cd4ed55973ce32eb2d54c1c28afd2d790a0b14
x0lotl <danil200319771@gmail.com>
2024-11-19 19:35:55 +0200

    feat(serde): base serialization

--- 6_serde/serde_practice/src/main.rs
+++ 6_serde/serde_practice/src/main.rs
@@ -1,3 +1,23 @@
+use serde::{Serialize, Deserialize};
+use serde_json;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct User {
+    name: String,
+    email: String,
+    birthdate: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user = User {
+        name: "Danylo".to_string(),
+        email: "danil200319771@gmail.com".to_string(),
+        birthdate: "2003-10-10".to_string(),
+    };
+
+    let json = serde_json::to_string(&user).expect("Serialize JSON failed");
+    println!("Serialized JSON: {}", json);
+
+    let deserialized_user: User = serde_json::from_str(&json).expect("Deserialize JSON failed");
+    println!("Deserialized JSON: {:?}", deserialized_user);
 }
