3cfd4e38d12e1b1c13349920118053dc2bea3597
x0lotl <danil200319771@gmail.com>
2024-11-20 18:37:04 +0200

    feat(calculator): finish calculator

--- 7_calculator/calculator/src/main.rs
+++ 7_calculator/calculator/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::io::BufRead;
+use pest::Parser;
+extern crate pest_derive;
+use crate::parser::{parse_expr, MyParser, Rule};
+
+mod parser;
+
+fn main() -> io::Result<()> {
+    for line in io::stdin().lock().lines() {
+        match MyParser::parse(Rule::equation, &line?) {
+            Ok(mut pairs) => {
+                let expr = parse_expr(pairs.next().unwrap().into_inner());
+                println!("Result: {}", expr.evaluate());
+            }
+            Err(e) => {
+                eprintln!("Parse failed: {:?}", e);
+            }
+        }
+    }
+    Ok(())
 }
